b29f8e7f2f23d6732b3024f2248d542313f0fbf1
tracymu <tracy.musung@moomumedia.com>
2014-03-06 18:22:52 +1100

    Scraping from Cathay

--- cathay_wombat.rb
+++ cathay_wombat.rb
@@ -5,14 +5,15 @@ flights = Wombat.crawl do
   path "/cx/en_AU/latest-offers.html"
   # Not sure how to do it from Sydney
 
-  results "css=.bullet-list", :iterator do
-    # The problem here being a) it is a list of countries and b) it is returning a string of cities and prices "from"
+  results "css=.offerPkgLst btm", :iterator do
+    city 'css=.ctry'
+    price 'css = .costfrom cost'
   end
 end
 
 print flights["results"]
 
-flights["results"].each do |result|
-  p "Is this happening?"
-end
+# flights["results"].each do |result|
+#   p "Is this happening?"
+# end
 
