ab19402ff9eb830515f9f2e052b14fb3ef058467
tracymu <tracy.musung@moomumedia.com>
2014-03-13 17:34:18 +1100

    Added commented out code about connecting Sinatra to a database

--- sinatra.rb
+++ sinatra.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
-# I also tried to use the shotgun gem...for this...but it didn't work this time.
+require 'datamapper'
+
+
+######  All this code commented out is to link Sinatra to a database  ###########
+# I didn't want to mess around with it too much in case I screwed it up, but ...
+# I include this for your information....
+# We will also need a new section of redis database for email and destinaton.
+
+# DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
+
+# class Note
+#   include DataMapper::Resource
+#   property :id, Serial
+#   property :content, Text, :required => true
+#   property :complete, Boolean, :required => true, :default => false
+#   property :created_at, DateTime
+#   property :updated_at, DateTime
+# end
+
+# DataMapper.finalize.auto_upgrade!
+
 
 get '/' do
   'Our Flights App'
@@ -12,3 +32,4 @@ end
 post '/form' do
   "Once we figure this out, we'll send you an email if flights to #{params[:destination]} go on sale!"
 end
+
